59bcc7b811b2bd0cf9df108a6bf7e2673d08308c
ranaat123 <rana.atalla5@gmail.com>
2025-01-22 12:58:29 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -8,9 +8,7 @@ def spread_fire(grid):
     for i in range(grid_size):
         for j in range(grid_size):
             if grid[i][j] == 1:
-                neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
-                if 2 in neighbors:
+                if (i>0 and [grid[i-1][j]==2) or (i<(grid_size-1) and [grid[i1][j]==2) or (j>0 and [grid[i][j-1]==2) or (j<(grid_size-1) and [grid[i][j+1]==2):
                     up_grid[i][j] = 2
 
     return up_grid
-
